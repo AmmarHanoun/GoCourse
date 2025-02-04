1c2153e7237baf3576e9fea41374a34ce2d602fc
AmmarHanoun <Ammar.Y.Hanoun198@Gmail.com>
2025-02-04 13:48:28 +0000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import(
+//  "booking/helper"
+ // "booking/secondPa"
+)
+
+func main(){
+ // creatMap1()
+ // creatMap2()
+//creatSliceOfMaps()
+  //creatSliceOfMaps2()
+ // helper.TestHelpPackage()
+  //secondPa.Second()
+ // forRange()
+  //reserveUsingDoubleSlice()
+  displayMap = map[string]map[string]map[uint]uint displayMap("1","London",10,10,displayMap)
+  //doubleSlice()
+ // switchCase()
+}
